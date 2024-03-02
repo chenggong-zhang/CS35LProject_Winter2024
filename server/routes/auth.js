71cb635c8030feedede63b14b194da24f2000d74
yiheng-zhu-ucla <yihengzhu@g.ucla.edu>
2024-03-02 00:03:16 -0800

    Update starter template code for routes

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -5,11 +5,22 @@ const router = express.Router();
 
 router.post('/email', (req, res) => {
 
+  // TODO: Implement email login/signup logic here
+  res.status(200).json({
+    ok: true,
+    message: `endpoint: ${req.baseUrl}/${req.route.path}`
+  });
+  
+});
+
+router.post('/email/verify', (req, res) => {
+
   // TODO: Implement email verification logic here
   res.status(200).json({
     ok: true,
+    message: `endpoint: ${req.baseUrl}/${req.route.path}`
   });
-  
+
 });
 
 module.exports = router;
