17fb474aa3706aaa68a1501a456035f6268a951d
Lionel-who <lionelhu20020926@gmail.com>
2024-03-03 17:51:42 -0800

    2nd version post component updated. Handled vibe status and play button and interaction button. Pending more refinement on dynamic components

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,25 @@
 import './App.css';
-import Mainpage from './mainpage.js'
+import Post from './Post/Post.jsx';
 
 function App() {
   return (
-      <Mainpage/>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <Post></Post>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
