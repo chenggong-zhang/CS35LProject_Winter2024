f6950683dbacefe177b150a1b19daeee1fef6ce6
yiheng-zhu-ucla <yihengzhu@g.ucla.edu>
2024-03-04 21:56:28 -0800

    Add auth route endpoints handling
    - /auth/email
    - /auth/email/verify
    - /auth/token
    - /auth/logout

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,44 +1,134 @@
 const express = require('express');
+const { authService, emailService,userService } = require('../services/index')
 
 const router = express.Router();
 
+// email login/signup with OTP email
+router.post('/email', async (req, res) => {
 
-router.post('/email', (req, res) => {
+  const { email } = req.body;
 
-  // TODO: Implement email login/signup logic here
-  res.status(200).json({
-    ok: true,
-    message: `endpoint: ${req.baseUrl}/${req.route.path}`
-  });
+  // check input data
+  if (!email) { 
+    return res.status(400).json({
+      ok: false,
+      error: 'email is missing'
+    });
+  }
+
+  // get or create user in database using email
+  const user = await userService.upsertUser({ email });
+
+  // send verification email to user
+  const emailSuccess = await emailService.sendOTPEmail(user);
+
+  if (!emailSuccess) {
+    // return error response
+    res.status(500).json({
+      ok: false,
+      error: 'failed to send OTP email'
+    });
+  } else {
+    // return success response
+    res.status(200).json({
+      ok: true,
+    });
+  }
   
 });
 
-router.post('/email/verify', (req, res) => {
+// email verification with OTP
+router.post('/email/verify', async (req, res) => {
+  const { email, otp } = req.body;
 
-  // TODO: Implement email verification logic here
-  res.status(200).json({
-    ok: true,
-    message: `endpoint: ${req.baseUrl}/${req.route.path}`
-  });
+  // check input data
+  if (!email) { 
+    return res.status(400).json({
+      ok: false,
+      error: 'email is missing'
+    });
+  }
+  if (!otp) { 
+    return res.status(400).json({
+      ok: false,
+      error: 'OTP is missing'
+    });
+  }
 
+  // verify OTP and login user
+  const verifyResult = await authService.emailOTPVerify({ email, otp });
+
+  if (!verifyResult.ok) {
+    // return error response
+    res.status(401).json({
+      ok: false,
+      error: verifyResult.error
+    });
+  } else {
+    // return success response
+    res.status(200).json({
+      ok: true,
+      user: verifyResult.user,
+      accessToken: verifyResult.accessToken,
+      refreshToken: verifyResult.refreshToken
+    });
+  }
 });
 
-router.get('/token', (req, res) => {
 
-  res.status(200).json({
-    ok: true,
-    message: `endpoint: ${req.baseUrl}/${req.route.path}`
-  });
+// refresh access token
+router.get('/token', async (req, res) => {
 
+  const { refreshToken } = req.query;
+
+  // check input data
+  if (!refreshToken) { 
+    return res.status(400).json({
+      ok: false,
+      error:'refreshToken is missing'
+    });
+  }
+
+  // refresh access token
+  const refreshResult = await authService.refreshAccessToken({ refreshToken });
+
+  if (!refreshResult.ok) {
+    // return error response
+    res.status(401).json({
+      ok: false,
+      error: refreshResult.error
+    });
+  } else {
+    // return success response
+    res.status(200).json({
+      ok: true,
+      accessToken: refreshResult.accessToken
+    });
+  }
 });
 
-router.post('/logout', (req, res) => {
 
-  res.status(200).json({
-    ok: true,
-    message: `endpoint: ${req.baseUrl}/${req.route.path}`
-  });
+// logout
+router.post('/logout', async (req, res) => {
 
+  const user_id = req.user.id;
+
+  // log user out
+  const logoutResult = await authService.logout({ user_id });
+
+  if (!logoutResult.ok) {
+    // return error response
+    res.status(404).json({
+      ok: false,
+      error: logoutResult.error
+    });
+  } else {
+    // return success response
+    res.status(200).json({
+      ok: true,
+    });
+  }
 });
+
 
 module.exports = router;
