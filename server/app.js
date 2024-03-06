df9812b2b0b36691405d37224fe67503202bd72d
yiheng-zhu-ucla <yihengzhu@g.ucla.edu>
2024-03-05 18:59:05 -0800

    Configure server and mongodb connection

--- server/app.js
+++ server/app.js
@@ -1,17 +1,24 @@
 require('dotenv').config();
 // require the express module
 const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+// connect to the database
+mongoose.connect(process.env.MONGODB_URI);
 
 // create an instance of the express app
 const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 const PORT = process.env.PORT || 5000;
 
 const routes = require('./routes');
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+// app.get('/', (req, res) => {
+//   res.send('Hello World!');
+// });
 
 // connect api routes
 app.use('/auth', routes.auth);
