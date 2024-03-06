687369cd6a6a4016cae7a014f03f6151026261c6
yiheng-zhu-ucla <yihengzhu@g.ucla.edu>
2024-03-05 16:38:55 -0800

    Implement relation route endpoints
    - /relation/:user_id
    - /relation/connect/:user_id
    - /relation/disconnect/:user_id

--- server/routes/relation.js
+++ server/routes/relation.js
@@ -1,34 +1,94 @@
 const express = require('express');
 
+const { relationService } = require('../services');
+
 const router = express.Router();
 
-router.get('/:user_id', (req, res) => {
+router.get('/:user_id', async (req, res) => {
+
+  const user_id = req.params.user_id;
+
+  // check if user_id exists
+  if(!user_id) {
+    return res.status(400).json({
+      ok: false,
+      error: 'user_id is missing'
+    });
+  }
+
+  const { followers, following } = await relationService.getRelatedUsers({userId: user_id});
 
   // TODO: Disconnect another user
   res.status(200).json({
     ok: true,
-    message: `endpoint: ${req.baseUrl}/${req.route.path}`
+    followers,
+    following
   });
     
 });
 
 
-router.post('/connect/:user_id', (req, res) => {
+router.post('/connect/:user_id', async (req, res) => {
 
-  // TODO: connect with another user
+  const other_user_id = req.params.user_id;
+  const user_id = req.user.id;
+
+  // check if other_user_id exists
+  if(!other_user_id) {
+    return res.status(400).json({
+      ok: false,
+      error: 'user_id (of the other user) is required'
+    });
+  }
+  // check if user_id and other_user_id are not the same
+  if(user_id === other_user_id) {
+    return res.status(400).json({
+      ok: false,
+      error: 'cannot connect with yourself'
+    });
+  }
+
+  const newRelation = await relationService.connectUser({selfUserId: user_id, friendUserId: other_user_id});
+
   res.status(200).json({
     ok: true,
-    message: `endpoint: ${req.baseUrl}/${req.route.path}`
+    relation: newRelation
   });
   
 });
 
-router.post('/disconnect/:user_id', (req, res) => {
+router.post('/disconnect/:user_id', async (req, res) => {
+
+  const other_user_id = req.params.user_id;
+  const user_id = req.user.id;
+
+  // check if other_user_id exists
+  if(!other_user_id) {
+    return res.status(400).json({
+      ok: false,
+      error: 'user_id (of the other user) is required'
+    });
+  }
+  // check if user_id and other_user_id are not the same
+  if(user_id === other_user_id) {
+    return res.status(400).json({
+      ok: false,
+      error: 'cannot disconnect with yourself'
+    });
+  }
+
+  const deletionAcknowledged = await relationService.disconnectUser({selfUserId: user_id, friendUserId: other_user_id});
+
+  if(!deletionAcknowledged) {
+    return res.status(400).json({
+      ok: false,
+      error: 'failed to disconnect users'
+    });
+  }
 
   // TODO: Disconnect another user
   res.status(200).json({
     ok: true,
-    message: `endpoint: ${req.baseUrl}/${req.route.path}`
   });
     
 });
