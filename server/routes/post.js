88b4f4ade366d8ef6614ccc3e9525c93642a64a3
yiheng-zhu-ucla <yihengzhu@g.ucla.edu>
2024-03-05 17:29:53 -0800

    Add post route endpoints implementation
    - POST /post
    - GET /post
    - POST /post/:post_id/reaction
    - GET /post/moods

--- server/routes/post.js
+++ server/routes/post.js
@@ -1,38 +1,101 @@
 const express = require('express');
 
+const { postService, relationService } = require('../services')
+
 const router = express.Router();
 
 // create a post
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
+
+  const { song, artists, mood } = req.body;
+  const user_id = req.user.id;
+
+  // check if song, artists, and mood are valid
+  if (!song) return res.status(400).json({ ok: false, error: 'song is missing'});
+  if (!artists) return res.status(400).json({ ok: false, error: 'artists is missing' });
+  if (!mood) return res.status(400).json({ ok: false, error:'mood is missing' });
+
+  // create a new post in the database
+  const newPost = await postService.createPost({ user_id, song, artists, mood });
 
   res.status(200).json({
     ok: true,
-    message: `endpoint: ${req.baseUrl}/${req.route.path}`
+    post: newPost
   });
   
 });
 
 
-// get a post
-router.get('/', (req, res) => {
+// get posts
+router.get('/', async (req, res) => {
+  const { sort, limit, offset, user_id: other_user_id } = req.query;
+  const user_id = req.user.id;
+  
+  if (other_user_id) {
+    // if other_user_id is provided, get posts from that user
+    const opts = { 
+      limit: limit || 20,
+      offset: offset || 0,
+      sort: { created_at: -1 }  // temporarily only sort by createdAt
+    };
+    const posts = await postService.getPostsByUser(other_user_id, opts);
 
-  res.status(200).json({
-    ok: true,
-    message: `endpoint: ${req.baseUrl}/${req.route.path}`
-  });
+    res.status(200).json({
+      ok: true,
+      posts
+    });
+
+  } else {
+    // get post feed for the current user
+    const opts = { 
+      limit: limit || 20,
+      offset: offset || 0,
+      sort: { created_at: -1 }  // temporarily only sort by createdAt
+    };
+
+    const followed_user_ids = await relationService.getUserIdsSelfFollows({ user_id });
+
+    const posts = await postService.getFeedPosts(followed_user_ids, opts);
+
+    res.status(200).json({
+      ok: true,
+      posts
+    });
+  }
     
 });
 
 
 // react to a post (toggle a reaction)
-router.post('/:post_id/reaction', (req, res) => {
+router.post('/:post_id/reaction', async (req, res) => {
+  const { post_id } = req.params;
+  const { reaction } = req.body;
+  const user_id = req.user.id;
 
+  // check post_id and reaction are valid
+  if (!post_id) return res.status(400).json({ ok: false, error: 'post_id is missing' });
+  if (!reaction) return res.status(400).json({ ok: false, error:'reaction is missing' });
+  if(!['like', 'handshake', 'fire', 'sad', 'lol', 'gg'].includes(reaction)) return res.status(400).json({ ok: false, error: 'invalid reaction' });
+
+  const updatedPost = await postService.reactToPost({ post_id, user_id, reaction });
+
+  res.status(200).json({
+    ok: true,
+    post: updatedPost
+  });
 })
 
 
 // get the latest trending moods
-router.get('/moods', (req, res) => {
-  
+router.get('/moods', async (req, res) => {
+  // get the latest (24h) trending moods
+  const sortedMoods = await postService.getTrendingMoods({ createdAfter: new Date(Date.now() - 24 * 60 * 60 * 1000) });
+
+  res.status(200).json({
+    ok: true,
+    moods: sortedMoods
+  });
+
 });
 
 module.exports = router;
