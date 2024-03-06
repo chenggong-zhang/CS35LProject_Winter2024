60c7012c3c5e3e86905df602bad8b422a6cc22e4
tianzezhao26@g.ucla.edu <160709907+Tianze0918@users.noreply.github.com>
2024-03-06 11:31:55 -0800

    temp changes

--- server/app.js
+++ server/app.js
@@ -1,9 +1,18 @@
 require('dotenv').config();
 // require the express module
 const express = require('express');
+// const cors = require('cors');
 
-// create an instance of the express app
-const app = express();
+// // create an instance of the express app
+// const app = express();
+// app.use(cors({
+//   origin: 'http://localhost:3000', // Specify the origin of requests that are allowed
+//   methods: ['POST', 'PUT', 'PATCH', 'GET', 'DELETE', 'OPTIONS'], // Specify the methods allowed
+// }));
+
+
+
+
 
 const PORT = process.env.PORT || 5000;
 
