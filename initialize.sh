b2477f5f4a9dadc359222df87a96e2668f07c162
yiheng-zhu-ucla <yihengzhu@g.ucla.edu>
2024-03-13 23:27:23 -0700

    Update init script to run server and client together

--- initialize.sh
+++ initialize.sh
@@ -3,15 +3,11 @@ echo "Installing dependencies for server..."
 cd server
 npm install
 
-echo "Running server..."
-npx nodemon app.js
-
 echo "Installing dependencies for client..."
 cd ../client
 npm install
 
-echo "Running client..."
-npm start
-
+cd ..
 echo "Initialization complete."
-echo "Open the client in your browser at http://localhost:3000"
+echo "running server and client..."
+npm start
